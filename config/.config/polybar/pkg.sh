bec248d1b2ca5a3bea51ac7285824ed1bc2f803d
Joseph Benden <joe@benden.us>
2019-09-20 15:59:58 -0700

    polybar: Fix script output formatting

--- config/.config/polybar/pkg.sh
+++ config/.config/polybar/pkg.sh
@@ -2,7 +2,7 @@
 apt=$(apt-get upgrade -qqsf | grep -E '^Inst' | cut -d ' ' -f 2 | wc -l)
 
 if [ $apt -gt 0 ]; then
-    echo " $apt updates..."
+    echo "%{F#900}%{F-} %{F#900}$apt%{F-} updates"
 else
-    echo ""
+    echo "%{F#666}%{F-}"
 fi
