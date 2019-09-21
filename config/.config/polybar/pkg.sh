b14db75ddf08adf0402fe5297f8d1824abbd85c6
Joseph Benden <joe@benden.us>
2019-09-20 21:19:28 -0700

    polybar: Handle missing apt-get
    Signed-off-by: Joseph Benden <joe@benden.us>

--- config/.config/polybar/pkg.sh
+++ config/.config/polybar/pkg.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+## Ensure APT is present
+if ! type apt-get >/dev/null 2>/dev/null; then
+	echo "Apt missing"
+	exit 0
+fi
+
 apt=$(apt-get upgrade -qqsf | grep -E '^Inst' | cut -d ' ' -f 2 | wc -l)
 
 if [ $apt -gt 0 ]; then
